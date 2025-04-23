8e30da0375050ff501883f2adf890d57efd8cbfe
xPreszo <iambrylletrinidad@gmail.com>
2025-04-23 11:26:46 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'a2dc8b297ab3753eabd3d66413fa4db3';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
